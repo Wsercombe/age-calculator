1d42b3dc8cf8447bfe9aba76c4ba46c0ff85cf6e
William Sercombe <wsercombe@gmail.com>
2021-07-31 16:02:04 -0700

    Add git ignore

--- src/index.js
+++ src/index.js
@@ -2,4 +2,15 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
-$(console.log("Hello World"));
+import AgeCalculator from './js/age-calculator';
+
+let ageCalculator = new AgeCalculator("mercury");
+
+
+
+$(document).ready(function() {
+  $('#age-calculator').submit(function(event) {
+    event.preventDefault();
+
+  });
+});
