78b95d3f8f5f4297cdbecc2d38751c3bde6d1c54
= <=>
2021-07-31 16:12:53 -0700

    testS

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -1,4 +1,12 @@
-function sum(a, b) {
-    return a + b;
+export default class AgeCalculator {
+  constructor(age, continent, lifeStyle, activityLevel) {
+    this.age = age;
+    this.continent = continent;
+    this.lifeStyle = lifeStyle;
+    this.activityLevel = activityLevel;
+    this.mercuryYears = this.parseAge(this.age);
+    this.venusYears = this.parseAge(age);
+    this.marsYears = this.parseAge(age);
+    this.jupterYears = this.parseAge(age);
   }
-  module.exports = sum;
+}
