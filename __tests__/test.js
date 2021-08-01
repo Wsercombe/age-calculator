a1c1d3fb1005a132ebc81c77b12e05cf5b74f681
= <=>
2021-07-31 22:49:06 -0700

    include mapping and testing for life expectancy based on continent

--- __tests__/test.js
+++ __tests__/test.js
@@ -1,7 +1,5 @@
 import AgeCalculator from '../src/js/age-calculator.js';
 
-
-
 describe('ageCalculator', () => {
   test('should initialize mercury years', () => {
     const mercuryAge = new AgeCalculator(0.24, "test", "test", "test");
@@ -20,5 +18,34 @@ describe('ageCalculator', () => {
     const jupiterAge = new AgeCalculator(11.86, "test", "test", "test");
     expect (jupiterAge.jupiterYears).toEqual(1);
     });
-        
+    
+  test('should determine life expectancy from continent based on antarctica', () => {
+    const antarcticaLifeExpectancy = new AgeCalculator(0, "antarctica", "", "");
+    expect (antarcticaLifeExpectancy.lifeExpectancy).toEqual(110);
+    });
+
+  test('should determine life expectancy from continent based on north america', () => {
+    const northAmericaLifeExpectancy = new AgeCalculator(0, "northAmerica", "", "");
+    expect (northAmericaLifeExpectancy.lifeExpectancy).toEqual(77);
+    });
+
+  test('should determine life expectancy from continent based on south america', () => {
+    const southAmericaLifeExpectancy = new AgeCalculator(0, "southAmerica", "", "");
+    expect (southAmericaLifeExpectancy.lifeExpectancy).toEqual(72);
+    });
+
+  test('should determine life expectancy from continent based on europe', () => {
+    const europeLifeExpectancy = new AgeCalculator(0, "europe", "", "");
+    expect (europeLifeExpectancy.lifeExpectancy).toEqual(75);
   });
+
+  test('should determine life expectancy from continent based on asia', () => {
+    const asiaLifeExpectancy = new AgeCalculator(0, "asia", "", "");
+    expect (asiaLifeExpectancy.lifeExpectancy).toEqual(71);
+  });
+
+  test('should determine life expectancy from continent based on africa', () => {
+    const africaLifeExpectancy = new AgeCalculator(0, "africa", "", "");
+    expect (africaLifeExpectancy.lifeExpectancy).toEqual(65);
+  });
+});
--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -8,5 +8,20 @@ export default class AgeCalculator {
     this.venusYears = this.age / 0.62;
     this.marsYears = this.age / 1.88;
     this.jupiterYears = this.age / 11.86;
+    this.lifeExpectancy = 0;
+    this.parseLifeExpectancy();
+  }
+  parseLifeExpectancy() {
+    // **based on made up data**
+    const continentMap = {
+      "northAmerica": 77,
+      "australia": 77,
+      "europe": 75,
+      "southAmerica": 72,
+      "asia": 71,
+      "africa": 65,
+      "antarctica": 110
+    };
+    this.lifeExpectancy = continentMap[this.continent];
   }
 }
