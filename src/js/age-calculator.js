d781bbdc50e6216dec6f2d3f149125b49f3f11ba
= <=>
2021-07-31 21:42:23 -0700

    Include planet map functionality

--- src/js/age-calculator.js
+++ src/js/age-calculator.js
@@ -4,9 +4,13 @@ export default class AgeCalculator {
     this.continent = continent;
     this.lifeStyle = lifeStyle;
     this.activityLevel = activityLevel;
-    this.mercuryYears = this.parseAge(this.age);
-    this.venusYears = this.parseAge(age);
-    this.marsYears = this.parseAge(age);
-    this.jupterYears = this.parseAge(age);
+    this.planetMap = this.parseAge(age);
+  }
+  parseAge(age) {
+    let planetMap = {};
+    planetMap["mercury"] = age / .24;
+    planetMap["venus"] = age / .62;
+    planetMap["mars"] = age / 1.88;
+    planetMap["jupiter"] = age / 11.86;
   }
 }
